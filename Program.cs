efdcff5739ce0dba2c52820fe6ee75c3d111f2d0
Mirgosoft <1498948+Mirgosoft@users.noreply.github.com>
2020-01-25 17:33:19 +0200

    Crash Notifies

--- Program.cs
+++ Program.cs
@@ -14,9 +14,37 @@ namespace AppResizer
         [STAThread]
         static void Main()
         {
+            // Detect Errors
+            AppDomain currentDomain = default(AppDomain);
+            currentDomain = AppDomain.CurrentDomain;
+            // Handler for unhandled exceptions.
+            currentDomain.UnhandledException += GlobalUnhandledExceptionHandler;
+            // Handler for exceptions in threads behind forms.
+            System.Windows.Forms.Application.ThreadException += GlobalThreadExceptionHandler;
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
+        private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = (Exception)e.ExceptionObject;
+            MessageBox.Show("Error: "+ ex.Message + "\n\n-------------\n\n" + ex.StackTrace, "Application Resizer Crashed =(");
+            Application.Exit();
+        }
+
+        private static void GlobalThreadExceptionHandler(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = e.Exception;
+            MessageBox.Show("Error: " + ex.Message + "\n\n-------------\n\n" + ex.StackTrace, "Application Resizer Crashed =(");
+            Application.Exit();
+        }
+
+
     }
 }
